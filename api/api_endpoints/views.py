3dd024f47d152e8842362b5c5851231517d0f3b5
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-08-09 13:34:47 +0300

    all good

--- api/api_endpoints/views.py
+++ api/api_endpoints/views.py
@@ -2,10 +2,11 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 
 # Create your views here.
-
+import os
 
 @api_view(['GET'])
 def api_welcome_message(request):
-    welcome_message = {'msg': 'Welcome to TheHub API'}
+    welcome_message = {'msg': 'Welcome to TheHub API and'+os.environ['PHONE_NUMBER']}
+
     return JsonResponse(welcome_message, status=200)
 
