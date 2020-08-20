0fb67a1951b3ffbc101d52f473c8f2febaf3998c
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-08-20 16:57:11 +0300

    added a modal for showing the LikeListComponent

--- frontend/src/profile-main-page/likes/like-list/LikeListComponent.js
+++ frontend/src/profile-main-page/likes/like-list/LikeListComponent.js
@@ -1,43 +1,69 @@
 import * as React from "react";
 import {PostService} from "../../../service/post-system/PostService";
 import {LikeComponent} from "../like-component/LikeComponent";
-
+import jQuery from 'jquery';
 
 export class LikeListComponent extends React.Component{
 
     state = {
         likesList: [],
-        isReady: false
+        isReady: false,
+        post_id: -1
     };
 
     constructor(props){
         super(props);
-        const {post_id} = props;
-        let postService = new PostService();
-        postService.retrieveLikes(post_id)
+
+    }
+
+    componentWillMount() {
+         let postService = new PostService();
+         const {post_id} = this.props;
+         postService.retrieveLikes(post_id)
             .then((result) => this.setState({
                 likesList: result.data.likes,
-                isReady: true
+                isReady: true,
+                post_id: post_id
             }))
             .catch((error) => console.log(error));
+    }
+
+    componentDidMount() {
+         window.jQuery(`#like-modal${this.state.post_id}`).modal('show')
     }
 
     render() {
         const {post_id} = this.props;
         return (
             <div>
-                {
-                    this.state.isReady &&
-                        this.state.likesList.map(
-                            (val, ind) =>{
-                                return (
-                                        <div key={`${post_id}-${val}`}>
-                                            <LikeComponent userName={val}/>
-                                        </div>
-                                )
-                            }
-                        )
-                }
+                <div className="modal fade" id={`like-modal${this.state.post_id}`} tabIndex="-1" role="dialog"
+                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLongTitle">People who like this post</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                {this.state.isReady &&
+                                    this.state.likesList.map(
+                                        (val, ind) =>{
+                                            return (
+                                                    <div key={`${post_id}-${val}`} style={{marginLeft: "1vw"}}>
+                                                        <LikeComponent userName={val}/>
+                                                    </div>
+                                            )
+                                        }
+                                    )
+                                }
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
--- frontend/src/profile-main-page/post-component/PostComponent.js
+++ frontend/src/profile-main-page/post-component/PostComponent.js
@@ -8,6 +8,7 @@ export class PostComponent extends React.Component{
     state = {
         postDetails: null
     };
+
     constructor(props){
         super(props);
     }
@@ -92,14 +93,6 @@ export class PostComponent extends React.Component{
                                                  {!showContent &&
                                                  <LikeListComponent post_id={this.state.postDetails.post_id}/>
                                                  }
-                                                 {!showContent &&
-                                                 <svg onClick={changeDisplayProperty} width="1em" height="1em"
-                                                      viewBox="0 0 16 16"
-                                                      className="bi bi-caret-up" fill="currentColor"
-                                                      xmlns="http://www.w3.org/2000/svg">
-                                                     <path fillRule="evenodd"
-                                                           d="M3.204 11L8 5.519 12.796 11H3.204zm-.753-.659l4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z"/>
-                                                 </svg>}
                                              </div>
                                          }
                                      </div>
