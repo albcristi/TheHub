524238598ccf804cc5a6aad41fa3bd07ca4eb84c
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-08-26 18:58:28 +0300

    Started handling the form input in the RegisterComponent

--- frontend/src/register/RegisterComponent.js
+++ frontend/src/register/RegisterComponent.js
@@ -1,11 +1,64 @@
 import * as React from "react";
 import './RegisterComponentStyle.css';
+import {UserService} from "../service/UserService";
 
 class RegisterComponent extends React.Component{
 
 
     constructor(props){
         super(props);
+    }
+
+    // HANDLE DATA RELATED TO USER NAME FIELD
+    handleUserNameInput(){
+        let formObject =  document.getElementById('box');
+        formObject = new FormData(formObject);
+        let typedUserName = formObject.get('usr_name');
+        if(typedUserName.length < 4){
+            // USER NAME SHOULD HAVE AT LEAST 4 CHARS
+            window.jQuery("#usr-name-msg").text('User name should contain at least 4 characters')
+        }
+        else{
+             let userService = new UserService();
+             userService.checkUserNameUnicity(typedUserName)
+                 .then((res) =>{
+                     if(!res.data.availability){
+                         window.jQuery("#usr-name-msg").text('User name already taken')
+                         return;
+                     }
+                     window.jQuery("#usr-name-msg").text('')
+                 })
+                 .catch((_) => {})
+        }
+    }
+
+    // HANDLE PASSWORD INPUT DATA
+    handelPasswordInput(){
+        let formObject =  document.getElementById('box');
+        formObject = new FormData(formObject);
+        let typedPassword = formObject.get('usr_password');
+        let errorMessage = "";
+        if(typedPassword.length <= 7){
+            errorMessage += 'Password length should at least 8 chars\n'
+        }
+        let regex1 = new RegExp('[0-9]+.*[A-Z]+');
+        let regex2 = new RegExp('[A-Z]+.*[0-9]+');
+        if(!regex1.test(typedPassword) && !regex2.test(typedPassword)){
+            errorMessage  += 'Password should include least one number and one capital letter\n';
+        }
+        if(errorMessage.length > 0){
+            window.jQuery("#pass-msg").text(errorMessage);
+            return;
+        }
+        window.jQuery("#pass-msg").text('');
+
+    }
+
+    // HANDLE RE-ENTERED PASSWORD
+    handleReEnteredPasswordInput(){
+        let formObject =  document.getElementById('box');
+        formObject = new FormData(formObject);
+        let typedPassword = formObject.get('usr_password');
     }
 
     registerClicked = (e) => {
@@ -19,11 +72,14 @@ class RegisterComponent extends React.Component{
                             <form  id="box" className="container" onSubmit={this.registerClicked}>
                                 <h1>Almost a member...</h1>
                                 <input type="text" name="usr_email" placeholder="Email" required/>
-                                <input type="text" name="usr_name" placeholder="Username" required/>
-                                <input type="password" name="usr_password" placeholder="Password" required/>
+                                <input type="text" onChange={() => {this.handleUserNameInput()}} name="usr_name" placeholder="Username" required/>
+                                <p id="usr-name-msg" className="text-danger"></p>
+                                <input  type="password" name="usr_password" placeholder="Password" onChange={()=>{this.handelPasswordInput()}} required/>
+                                <p id="pass-msg" className="text-danger"></p>
                                 <input type="password" name="re_usr_password" placeholder="Enter password again" required/>
+                                <p id="re-pass-msg" className="text-danger"></p>
                                 <input type="text" name="phone-number" placeholder="Phone Number" required/>
-                                <p id="form-message" className="text-danger text-center"></p>
+                                <p id="phone-msg" className="text-danger"></p>
                                 <input type="submit" name="" value="Register"/>
                             </form>
 
--- frontend/src/service/UserService.js
+++ frontend/src/service/UserService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export class UserService{
-     BASE_URL = 'http:://localhost:8000/api/user/';
+     BASE_URL = 'http://localhost:8000/api/user/';
 
      checkUserNameUnicity(userName){
          return axios.get(
