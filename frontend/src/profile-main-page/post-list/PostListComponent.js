55d4071c3c4f1183cecb06b71515cf88e2df48a4
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-08-31 18:30:17 +0300

    started working on the post pagination show

--- frontend/src/profile-main-page/post-list/PostListComponent.js
+++ frontend/src/profile-main-page/post-list/PostListComponent.js
@@ -1,14 +1,15 @@
 import * as React from "react";
 import {PostComponent} from "../post-component/PostComponent";
 import {PostService} from "../../service/post-system/PostService";
-
+import './PostListStyle.css';
 
 export class PostListComponent extends React.Component{
 
     state = {
-        pageNo: 0,
+        pageNo: 1,
         activePosts: [],
-        isReady: false
+        isReady: false,
+        noPerPage: 3
     };
 
     constructor(props) {
@@ -19,34 +20,66 @@ export class PostListComponent extends React.Component{
     componentDidMount() {
         const p = new PostService();
         p
-            .retrieve_related_posts(1,5)
+            .retrieve_related_posts(this.state.pageNo, this.state.noPerPage)
             .then((res)=>{
                 let posts = res.data.result;
                 posts.sort((post1, post2) => post2.post_id - post1.post_id);
-                this.setState({pageNo: this.state.pageNo,
+                this.setState({
+                    pageNo: this.state.pageNo+1,
                     activePosts: posts,
-                    isReady: true})
+                    isReady: true,
+                    noPerPage: this.state.noPerPage})
+
             })
             .catch((_)=>{});
 
     }
 
 
+    loadMorePosts(state){
+        let postService = new PostService();
+        postService.retrieve_related_posts(state.pageNo, state.noPerPage)
+            .then((res) =>{
+                let posts = res.data.result;
+                posts.sort((post1, post2) => post2.post_id - post1.post_id);
+                this.setState({
+                    pageNo: state.pageNo+1,
+                    activePosts: [...state.activePosts, ...posts],
+                    isReady: true,
+                    noPerPage: state.noPerPage
+
+                });
+                 console.log(posts)
+            })
+            .catch((_)=>{})
+    }
+
     render() {
 
         return (
             <div>
-                { this.state.isReady &&
-                    this.state.activePosts.map(
-                        (val, index) =>{
-                            return (
-                                <div key={index}>
-                                    <PostComponent postDetail={val} />
-                                </div>
-                            )
-                        }
-                    )
-                }
+                <div id="list-of-post">
+                    { this.state.isReady &&
+                        this.state.activePosts.map(
+                            (val, index) =>{
+                                return (
+                                    <div key={index}>
+                                        <PostComponent postDetail={val} />
+                                    </div>
+                                )
+                            }
+                        )
+                    }
+                </div>
+                <div style={{paddingTop: "10px", paddingBottom: "20px"}}>
+                    <svg onClick={()=>{this.loadMorePosts(this.state)}} id="show-more-posts-svg" width="3em" height="3em" viewBox="0 0 16 16" className="bi bi-arrow-down-circle"
+                         fill="gray" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd"
+                              d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path fillRule="evenodd"
+                              d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                </div>
             </div>
         )
     }
