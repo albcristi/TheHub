cdb8b78e866e59df3f6da3e4c386e2f9b81379d3
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-08-31 18:38:27 +0300

    Kinda done with the pagination for posts

--- frontend/src/profile-main-page/post-list/PostListComponent.js
+++ frontend/src/profile-main-page/post-list/PostListComponent.js
@@ -9,7 +9,8 @@ export class PostListComponent extends React.Component{
         pageNo: 1,
         activePosts: [],
         isReady: false,
-        noPerPage: 3
+        noPerPage: 3,
+        hasMore: true
     };
 
     constructor(props) {
@@ -28,7 +29,8 @@ export class PostListComponent extends React.Component{
                     pageNo: this.state.pageNo+1,
                     activePosts: posts,
                     isReady: true,
-                    noPerPage: this.state.noPerPage})
+                    noPerPage: this.state.noPerPage,
+                    hasMore: posts.length > 0})
 
             })
             .catch((_)=>{});
@@ -46,7 +48,8 @@ export class PostListComponent extends React.Component{
                     pageNo: state.pageNo+1,
                     activePosts: [...state.activePosts, ...posts],
                     isReady: true,
-                    noPerPage: state.noPerPage
+                    noPerPage: state.noPerPage,
+                    hasMore: posts.length > 0
 
                 });
                  console.log(posts)
@@ -71,15 +74,22 @@ export class PostListComponent extends React.Component{
                         )
                     }
                 </div>
-                <div style={{paddingTop: "10px", paddingBottom: "20px"}}>
-                    <svg onClick={()=>{this.loadMorePosts(this.state)}} id="show-more-posts-svg" width="3em" height="3em" viewBox="0 0 16 16" className="bi bi-arrow-down-circle"
-                         fill="gray" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path fillRule="evenodd"
-                              d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
-                    </svg>
-                </div>
+                {this.state.hasMore &&
+                    <div style={{paddingTop: "10px", paddingBottom: "20px"}}>
+                        <svg onClick={()=>{this.loadMorePosts(this.state)}} id="show-more-posts-svg" width="3em" height="3em" viewBox="0 0 16 16" className="bi bi-arrow-down-circle"
+                             fill="gray" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd"
+                                  d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                            <path fillRule="evenodd"
+                                  d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
+                        </svg>
+                    </div>
+                }
+                {!this.state.hasMore &&
+                    <div style={{paddingTop: "10px", paddingBottom: "20px"}} className="text-center text-info">
+                        <h4>There are no other posts available for the moment</h4>
+                    </div>
+                }
             </div>
         )
     }
