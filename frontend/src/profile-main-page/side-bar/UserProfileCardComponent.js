8f40a16a47b2c0c961f7a157f0d1e8b05fdb80ba
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-08-31 17:23:59 +0300

    did some styling for the profile pagew

--- frontend/src/profile-main-page/side-bar/UserProfileCardComponent.js
+++ frontend/src/profile-main-page/side-bar/UserProfileCardComponent.js
@@ -7,7 +7,7 @@ export class UserProfileCardComponent extends React.Component{
         return (
             <div style={{display: "unset", height: "auto"}} className="profileSideBarCard">
                 <div id="side-bar-nav" className="bg-light">
-                    <div className="sidebar-heading"><strong><h2 className="text-center text-primary">@{sessionStorage.getItem('user_name')}</h2></strong></div>
+                    <div className="sidebar-heading"><strong><h2 className="text-center text-primary user-name-side-bar">@{sessionStorage.getItem('user_name')}</h2></strong></div>
                     <div className="list-group list-group-flush">
                             <div  id="profile-card-options" className="d-flex flex-column">
                                     <div className="logo-side-bar">
